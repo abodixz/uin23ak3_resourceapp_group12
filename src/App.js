73007561bbc171d3a173e9bf62d1d6259b4f229e
bobauser <edvin.ulvestad@hiof.no>
2023-03-08 23:19:43 +0100

    created routing for components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './css/main.css';
 import Button from './components/Button';
+import {Route, Routes} from 'react-router-dom'
 import Layout from './components/Layout';
 import Resources from './components/Resources';
 
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout/>} >
+        <Route exact path='/' element={<Resources category={'html'} />} />
+        <Route path='/html' element={<Resources category={'html'} />}/>
+        <Route path='/css' element={<Resources category={'css'} />}/>
+        <Route path='/javascript' element={<Resources category={'javascript'} />}/>
+        <Route path='/react' element={<Resources category={'react'} />}/>
+        <Route path='/headless-cms' element={<Resources category={'headless-cms'} />}/>
+        <Route element={<Button/>}/>
+      </Route>
+    </Routes>
   );
 }
-
 export default App;
